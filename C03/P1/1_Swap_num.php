fb176a0e0e99f3c8e48784cf4decb84c187ef82c
Rabah Ahmed <88627155+Silvie3D@users.noreply.github.com>
2025-12-02 20:42:14 +0530

    co3 fix

--- C03/P1/1_Swap_num.php
+++ C03/P1/1_Swap_num.php
@@ -4,15 +4,12 @@
             $a=10;
             $b=20;
             $temp;
-            
             print("Before Swapping: a=$a,b=$b");
             $temp=$a;
             $a=$b;
             $b=$temp;
-
             print("\nAfter swapping: a=$a,b=$b\n");
         ?> 
-
     </body>
 </html>
 
--- C03/P2/2_Armstrong.php
+++ C03/P2/2_Armstrong.php
@@ -3,13 +3,10 @@
         <?php
             $number=153;
             $num_array=str_split(strval($number));
-
             $sum=0;
-
             for($i=0;$i<count($num_array);$i++){
                 $sum+=(int)$num_array[$i]**3;
             }
-
             if($sum==$number){
                 print("$number is an armstrong number\n");
             }else{
