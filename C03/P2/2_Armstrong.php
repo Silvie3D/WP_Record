14d9557e1f2eb7c85dea379afa31831dad0e2074
Rabah Ahmed <88627155+Silvie3D@users.noreply.github.com>
2025-12-08 22:36:13 +0530

    small fixes

--- C03/P2/2_Armstrong.php
+++ C03/P2/2_Armstrong.php
@@ -1,16 +1,25 @@
 <html>
     <body>
+        <h2>Armstrong Number Checker</h2>
+        <form method="POST">
+            <label for="number">Enter a number:</label>
+            <input type="number" id="number" name="number" required>
+            <button type="submit">Check</button>
+        </form>
+        
         <?php
-            $number=153;
-            $num_array=str_split(strval($number));
-            $sum=0;
-            for($i=0;$i<count($num_array);$i++){
-                $sum+=(int)$num_array[$i]**3;
-            }
-            if($sum==$number){
-                print("$number is an armstrong number\n");
-            }else{
-                print("$number is not an armstrong number\n");
+            if(isset($_POST['number'])){
+                $number=$_POST['number'];
+                $num_array=str_split(strval($number));
+                $sum=0;
+                for($i=0;$i<count($num_array);$i++){
+                    $sum+=(int)$num_array[$i]**3;
+                }
+                if($sum==$number){
+                    print("<p>$number is an armstrong number</p>");
+                }else{
+                    print("<p>$number is not an armstrong number</p>");
+                }
             }
         ?>
     </body>
